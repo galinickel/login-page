40a4a372ff4354b1ba9f32c740d6406962433a22
Gali Nickel <gali.nickel95@gmail.com>
2021-06-17 19:59:29 +0300

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<GalisButton
+		text="כניסה"
+			color="#18c746"
+			:isLoading="isButtonLoading"
+			@click.native="toggleLoader"
+		/>
+		<GalisInput :userInput="userInput" />
+	</div>
 </template>
+
+<script>
+import { GalisButton, GalisInput } from 'gali-package'
+export default {
+	components: { GalisButton, GalisInput },
+	data() {
+		return {
+			isButtonLoading: false,
+			userInput: {
+				name: 'מייל',
+				type: 'text',
+				validation: (value) => { return value.length < 5 && value.length > 1 },
+				invalidMsg: 'כתובת המייל איתה נרשמת לחשבונית ירוקה'
+			}
+		}
+	},
+	methods: {
+		toggleLoader() {
+			this.isButtonLoading = true
+			setTimeout(() => {
+				this.isButtonLoading = false
+			}, 1500);
+		}
+	}
+}
+</script>
